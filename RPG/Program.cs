bb59aba0fb086eff0bf7462248f2b5db9501c8af
Tristan-W-meow <tw2mq@students.coconino.edu>
2025-11-11 21:50:45 -0700

    Changed some things to floats because decimals will almost defintiely be used

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -1,6 +1,7 @@
 ﻿using RPG;
-using System.Runtime.CompilerServices;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 namespace RPG
 {
     internal class Program
@@ -12,8 +13,11 @@ namespace RPG
 
             Intro.DisplayIntro();
             Character hero = new Character("Archer", 1, 100);
+         
 
-            Console.WriteLine(Component.swordArm.ComponentName);
+            Console.WriteLine($"Character Created: {hero.Name}, Level: {hero.Level}, Health: {hero.Health}");
+
+            Console.WriteLine();
         }
     }
     public class Character
@@ -27,6 +31,8 @@ namespace RPG
             Level = level;
             Health = health;
         }
+
+
         public void Attack(Character target)
         {
             Console.WriteLine($"{Name} attacks {target.Name}!");
@@ -67,40 +73,42 @@ namespace RPG
 
     public class Stats
     {
-        public int strength { get; set; }
-        public int resilience { get; set; }
-        public int agility { get; set; }
-        public int speed { get; set; }
-        public int power_generation { get; set; }
-        public int power { get; set; }
-        public int processing_power { get; set; }
-        public int encryption { get; set; }
-        public  Stats(int strength, int resilience, int agility, int speed, int power_generation, int power, int processing_power, int encryption)
+        public float Strength { get; set; }
+        public float Resilience { get; set; }
+        public float Agility { get; set; }
+        public float Speed { get; set; }
+        public float Power_generation { get; set; }
+        public float Power { get; set; }
+        public float Processing_power { get; set; }
+        public float Encryption { get; set; }
+        public  Stats(float strength, float resilience, float agility, float speed, float power_generation, float power, float processing_power, float encryption)
         {
-            this.strength = strength;
-            this.resilience = resilience;
-            this.agility = agility;
-            this.speed = speed;
-            this.power_generation = power_generation;
-            this.power = power;
-            this.processing_power = processing_power;
-            this.encryption = encryption;
+            Strength = strength;
+            Resilience = resilience;
+            Agility = agility;
+            Speed = speed;
+            Power_generation = power_generation;
+            Power = power;
+            Processing_power = processing_power;
+            Encryption = encryption;
         }
 
     }
     public class Component
     {
         public string ComponentName { get; set; }
-        public int Efficiency { get; set; }
-        public int Durability { get; set; }
-        public int Power_cosumption { get; set; }
-        public int Weight { get; set; }
-        public int Stat_mod1 { get; set; }
-        public int Stat_mod2 { get; set; }
+        public float Structure { get; set; }
+        public float Efficiency { get; set; }
+        public float Durability { get; set; }
+        public float Power_cosumption { get; set; }
+        public float Weight { get; set; }
+        public float Stat_mod1 { get; set; }
+        public float Stat_mod2 { get; set; }
 
-        public Component(string conponentName, int durability, int efficiency, int power_cosumption, int weight, int stat_mod1, int stat_mod2)
+        public Component(string conponentName, float structure, float durability, float efficiency, float power_cosumption, float weight, float stat_mod1, float stat_mod2)
         {
             ComponentName = conponentName;
+            Structure = structure;
             Efficiency = efficiency;
             Durability = durability;
             Power_cosumption = power_cosumption;
@@ -109,18 +117,17 @@ namespace RPG
             Stat_mod2 = stat_mod2;
 
         }
+        public void SwordArm()
+        {
+            Component swordArm = new Component("Sword Arm", 100, 1, 90, 20, 15, 5, 3);
+        }
 
-        public Component swordArm = new Component("Sword Arm", 100, 80, 20, 15, 5, 3);
 
         public void DisplayInfo()
         {
             Console.WriteLine($"Durability: {Durability}, Efficiency: {Efficiency}, Power Consumption: {Power_cosumption}, Weight: {Weight}, Stat Mod 1: {Stat_mod1}, Stat Mod 2: {Stat_mod2}");
         }
 
-        public void SwordArm()
-        {
-
-        }
 
     }
     public class Intro
@@ -158,8 +165,49 @@ namespace RPG
             }
         }
     }
-    public class 
- }
+    public class Encounter
+    {
+
+        public static void DisplayCombatOptions()
+        {
+            Console.WriteLine("What do you wish to activate?");
+            Console.WriteLine("1. Attack");
+            Console.WriteLine("2. Defend");
+            Console.WriteLine("3. Use Item");
+        }
+        public static void StartCombat(Character player, Character enemy)
+        {
+            Console.WriteLine($"A wild {enemy.Name} appears!");
+            while (player.Health > 0 && enemy.Health > 0)
+            {
+                player.Attack(enemy);
+                if (enemy.Health <= 0)
+                {
+                    Console.WriteLine($"{enemy.Name} has been defeated!");
+                    break;
+                }
+                enemy.Attack(player);
+                if (player.Health <= 0)
+                {
+                    Console.WriteLine($"{player.Name} has been defeated!");
+                    break;
+                }
+            }
+        }
+        public int CombatTime(int Time, int Speed)
+        {
+            while (Time <100)
+            {
+                Time += Speed;
+                if (Time >= 100)
+                {
+                    Time -=100 ;
+                    return Time;
+                }
+            }
+        }
+    }
+}
 
     
     
